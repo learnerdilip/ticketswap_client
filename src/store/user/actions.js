c2903c5c7ec35ba56cb0bf4ee48cf60726eb3010
learnerdilip <diliptalks@gmail.com>
2020-04-01 01:57:47 +0200

    changed the link to server

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = `http://localhost:4000`;
+const baseUrl = `https://devdilip-ticketswap-server.herokuapp.com/`;
 
 function userLoggedIn(data) {
   return {
