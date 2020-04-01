90ea07e4035aca19e1f621a949ec7adcfb200926
learnerdilip <diliptalks@gmail.com>
2020-04-01 02:02:51 +0200

    added a slash to url

--- src/store/events/actions.js
+++ src/store/events/actions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = `https://devdilip-ticketswap-server.herokuapp.com/`;
+const baseUrl = `https://devdilip-ticketswap-server.herokuapp.com`;
 
 const eventCreated = data => {
   return {
